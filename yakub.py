0189362f7d9f3debb3a849f346fd3e548c99956d
yusufsho14@gmail.com <60248388+YakubYakubov@users.noreply.github.com>
2020-01-25 12:08:52 -0500

    Readme.md

---
+++ yakub.py
@@ -0,0 +1,10 @@
+  151  history
+  152  git checkout -b yakub
+  153  git branch
+  154  git checkout -b yakub
+  155  git checkout branch
+  156  git yakub
+  157  history tail n/10
+  158  history | tail
+  159  cd projekt1
+  160  history | tail>>yakub.py
